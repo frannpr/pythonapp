5caca93b5464e374f6a72eaacf56c6a4e6d929bc
franpr09 <70898127+franpr09@users.noreply.github.com>
2020-11-08 20:12:13 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return """
-    <h1>Prueba de Python en Docker!</h1>
-    """
+   return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
